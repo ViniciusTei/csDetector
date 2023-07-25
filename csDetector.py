9ed665e1e752dafb3d6f2261d19ab29df5f1f178
ViniciusTei <viniciustprates@gmail.com>
2023-07-25 08:37:38 -0300

    wip: init app loggin

--- csDetector.py
+++ csDetector.py
@@ -1,7 +1,9 @@
-from csDetector import csDetector
+from csDetector import csDetector, logger
 import sys
 
 if __name__ == "__main__":
+    logger = logger.Logger()
+    logger.log("type","Started application")
     inputData = sys.argv[1:]
     tool = csDetector()
     formattedResults, results, config = tool.executeTool(inputData)
