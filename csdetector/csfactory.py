6546e4135ee00f5c13e5060b0a4736effe87f471
ViniciusTei <viniciustprates@gmail.com>
2023-08-24 08:30:18 -0300

    feat add senti strength to config

--- csdetector/csfactory.py
+++ csdetector/csfactory.py
@@ -1,4 +1,5 @@
 import os
+import sentistrength
 from git.repo import Repo
 
 from csdetector import Configuration, utils
@@ -7,6 +8,8 @@ from csdetector.metrics.authorAlias import AuthorAlias
 class CSFactory:
     _config: Configuration
     _repo: Repo
+    _senti: sentistrength.PySentiStr
+
     def __init__(self, config: Configuration):
         self._config = config
         
@@ -18,6 +21,17 @@ class CSFactory:
 
         # get repository reference
         self._repo = utils.getRepo(self._config)
+        
+        # setup sentiment analysis
+        self._senti = sentistrength.PySentiStr()
+
+        sentiJarPath = os.path.join(
+            config.sentiStrengthPath, "SentiStrength.jar").replace("\\", "/")
+        self._senti.setSentiStrengthPath(sentiJarPath)
+
+        sentiDataPath = os.path.join(
+            config.sentiStrengthPath, "SentiStrength_Data").replace("\\", "/") + "/"
+        self._senti.setSentiStrengthLanguageFolderPath(sentiDataPath)
 
     def detect(self):
         authorAliasExtractor = AuthorAlias(self._config, self._repo)
